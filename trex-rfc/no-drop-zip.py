0538e4b046589e31493eb7db8ab059c84f1f5554
VladimiroPaschali <vladimiropaschali@gmail.com>
2026-04-13 09:12:01 +0000

    perf per packet

--- trex-rfc/no-drop-zip.py
+++ trex-rfc/no-drop-zip.py
@@ -20,20 +20,20 @@ PORTS = [0]  # single port loopback
 
 def test_loopback(client):
 
-    print("Test veloce loopback (sanity check)...")
-    client.start(ports=PORTS, mult="10pps", duration=5)
+    tqdm.write("Test veloce loopback (sanity check)...")
+    client.start(ports=PORTS, mult="10pps", duration=5, force=True)
     client.wait_on_traffic(ports=PORTS)
     stats = client.get_stats()[0]
     tx = stats["opackets"]
     rx = stats["ipackets"]
-    print(f"TX={tx} RX={rx}")
+    tqdm.write(f"TX={tx} RX={rx}")
     if rx == 0:
         raise RuntimeError("❌ Nessun traffico di ritorno! Loopback NON funzionante")
 
     # if abs(tx - rx) > 10:
     #     raise RuntimeError(f"❌ Differenza TX/RX troppo alta: {tx-rx}")
 
-    print("✅ Loopback OK\n")
+    tqdm.write("✅ Loopback OK\n")
 
 def _parse_perf_output(perf_output: str) -> dict:
 
@@ -47,19 +47,52 @@ def _parse_perf_output(perf_output: str) -> dict:
             perf_data[event] = int(number)
     return perf_data
 
+def _get_dpdk_throughput(dpdk_output) -> int:
+
+    match = re.search(r'measured RX Throughput:\s*(\d+)', dpdk_output)
+    if match:
+        rx_packets = int(match.group(1))
+        # tqdm.write(f"Throughput: {rx_packets}")
+    else:
+        rx_packets = -1
+        tqdm.write("RX packets not found.")
+    
+    return rx_packets
+
+def _get_all_pkts(dpdk_output) -> int:
+
+    match = re.search(r'measured RX packets:\s*(\d+)', dpdk_output)
+    if match:
+        rx_packets = int(match.group(1))
+        # tqdm.write(f"RX packets: {rx_packets}")
+    else:
+        rx_packets = -1
+        tqdm.write("RX packets not found.")
+    
+    return rx_packets
+
 def launch_program_with_perf(base_command, queue, policy, perf_events):
     perf_events_str = ','.join(perf_events)
     perf_prefix = f'sudo perf stat -C 4 -e {perf_events_str} --timeout 20000'
     command = f'{perf_prefix} {base_command} -q {queue} {policy}'
+    # tqdm.write("Running command with perf:" + command)
 
     try:
         result = sp.run(shlex.split(command), capture_output=True, text=True, check=True)
 
     except sp.CalledProcessError as e:
-        print("Error running perf command:", e)
+        tqdm.write("Error running perf command:" + str(e))
         return {}
     
+    # tqdm.write(result.stdout)
     perf_data = _parse_perf_output(result.stderr)
+    all_pkts = _get_all_pkts(result.stdout)
+    throughput = _get_dpdk_throughput(result.stdout)
+    if all_pkts <= 0:
+        tqdm.write("⚠️ All packets count is zero or negative, cannot normalize perf data.")
+    else:
+        perf_data_per_pkt = {k: v/ all_pkts for k, v in perf_data.items()}
+        perf_data = perf_data_per_pkt
     return perf_data
 
 
@@ -100,7 +133,7 @@ def launch_program(base_command, queue, policy):
             return -1
 
     except Exception as e:
-        print("Exception occurred while running command:", str(e))
+        tqdm.write("Exception occurred while running command:" + str(e))
         return -1
 
 def stop_program(process):
@@ -115,7 +148,7 @@ def extract_final_pps(bench, policy, queue, repetition):
     try:
         results_json = bench.results.to_json()
         if not results_json:
-            print(f"⚠️ Nessun risultato disponibile per queue={queue}, policy='{policy}', rep={repetition}")
+            tqdm.write(f"⚠️ Nessun risultato disponibile per queue={queue}, policy='{policy}', rep={repetition}")
             return None
         
         # Normalizza i dati da JSON
@@ -130,9 +163,9 @@ def extract_final_pps(bench, policy, queue, repetition):
         # Estrai rx_pps
         rx_pps = results_data.get('rx_pps', None)
         
-        return rx_pps
+        return round(rx_pps , 2)
     except Exception as e:
-        print(f"❌ Errore nell'estrazione pps: {e}")
+        tqdm.write(f"❌ Errore nell'estrazione pps: {e}")
         return None
 
 
@@ -141,7 +174,7 @@ def export_results_to_csv(bench, policy, queue, repetition, perf_metrics, perf_e
     try:
         results_json = bench.results.to_json()
         if not results_json:
-            print(f"⚠️ Nessun risultato disponibile per queue={queue}, policy='{policy}', rep={repetition}")
+            tqdm.write(f"⚠️ Nessun risultato disponibile per queue={queue}, policy='{policy}', rep={repetition}")
             return
         
         # Normalizza i dati da JSON
@@ -188,14 +221,14 @@ def export_results_to_csv(bench, policy, queue, repetition, perf_metrics, perf_e
         
         tqdm.write(f"✅ Risultati + perf salvati in {csv_file}")
     except Exception as e:
-        print(f"❌ Errore nell'esportazione CSV: {e}")
+        tqdm.write(f"❌ Errore nell'esportazione CSV: {e}")
 
 
 def calculate_and_export_averages(perf_events, csv_file="benchmark_results.csv", averages_csv="benchmark_averages.csv"):
     """Calcola le medie per ogni gruppo di ripetizioni (policy, queue) e le esporta in un CSV separato"""
     try:
         if not os.path.isfile(csv_file):
-            print(f"⚠️ File {csv_file} non trovato")
+            tqdm.write(f"⚠️ File {csv_file} non trovato")
             return
         
         # Leggi il CSV dei risultati
@@ -205,7 +238,7 @@ def calculate_and_export_averages(perf_events, csv_file="benchmark_results.csv",
             data = list(reader)
         
         if not data:
-            print(f"⚠️ Nessun dato trovato in {csv_file}")
+            tqdm.write(f"⚠️ Nessun dato trovato in {csv_file}")
             return
         
         # Raggruppa per policy e queue
@@ -272,30 +305,30 @@ def calculate_and_export_averages(perf_events, csv_file="benchmark_results.csv",
                 writer.writeheader()
                 writer.writerows(averages)
             
-            print(f"✅ Medie e deviazioni standard salvate in {averages_csv} | policy='{policy}', queue={queue}")
+            tqdm.write(f"✅ Medie e deviazioni standard salvate in {averages_csv} | policy='{policy}', queue={queue}")
         else:
-            print(f"⚠️ Nessuna media da calcolare")
+            tqdm.write(f"⚠️ Nessuna media da calcolare")
     
     except Exception as e:
-        print(f"❌ Errore nel calcolo delle medie: {e}")
-    
+        tqdm.write(f"❌ Errore nel calcolo delle medie: {e}")
+
 def launch_trex(client, pps):
     client.start(ports=PORTS, mult=f"{pps}pps", force=True)
     # aspetta wamup traffico a pps
-    print("Waiting for traffic to reach expected rate before stopping warmup...")
+    tqdm.write("Waiting for traffic to reach expected rate before stopping warmup...")
 
     while True:
         stats = client.get_stats(ports=[0])
         current_pps = stats[0]['tx_pps']
         if current_pps >= int(pps) * 0.9:  # check if traffic is at least 90% of expected
             # print(f"Traffic is at {current_pps} pps, stopping warmup.")
-            print(f"Traffic reached {current_pps} pps, stopping warmup.")
+            tqdm.write(f"Traffic reached {current_pps} pps, stopping warmup.")
             break
         sleep(1)
     return
 
 def stop_trex(client):
-    print("Stopping TRex traffic...")
+    tqdm.write("Stopping TRex traffic...")
     client.stop(ports=PORTS)
     while True:
         stats = client.get_stats(ports=PORTS)
@@ -303,42 +336,50 @@ def stop_trex(client):
             break
         sleep(1)
 
-    print("Traffic fully stopped.")
+    tqdm.write("Traffic fully stopped.")
 
 def clear_csv_files():
     """Rimuove i file CSV esistenti per risultati e medie"""
     for csv_file in ["benchmark_results.csv", "benchmark_averages.csv"]:
         if os.path.isfile(csv_file):
             os.remove(csv_file)
-            print(f"🔄 CSV precedente rimosso ({csv_file})")
+            tqdm.write(f"🔄 CSV precedente rimosso ({csv_file})")
 
 def main():
     client = STLClient(server="10.181.120.102")
     repetitions = 3
     queues = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+    zipf_skews = [0.6, 0.9]
+    # queues = [1, 64, 256, 512, 1024]
+    # repetitions = 1
+    # zipf_skews = [0.6]
+
+
     
     # Definisci i comandi e le loro configurazioni
     commands_config = {
-        'chain': {
-            'policies': ["", "-a", "-a -s 5", "-s 20", "-a -s 20", "-s 50"],
-            'base_command': 'sudo /home/vladimiro/dpdk_patched/queueDPDK/asni-nat/build/asni-nat -d librte_net_qdma.so -l 4 -n 4 -a 0000:16:00.1 -- -p 0x3 --config="(0,0,4)" --rule_ipv4=../chain/fw_10k --rule_ipv6=../chain/rule_ipv6.db'
-        },
-        'marco_chain': {
-            'policies': ["", "-a"],
-            'base_command': 'sudo /home/vladimiro/dpdk_patched/marco/dpdk-20.11/queueDPDK/asni-nat/build/asni-nat -d librte_net_qdma.so -l 4 -n 4 -a 0000:16:00.1,indirect_queues=1024 -- -p 0x3 --config="(0,0,4)" --rule_ipv4=../chain/fw_10k --rule_ipv6=../chain/rule_ipv6.db'
-        },
+        # 'chain': {
+        #     'policies': ["", "-a", "-a -s 5", "-s 20", "-a -s 20", "-s 50"],
+        #     'base_command': 'sudo /home/vladimiro/dpdk_patched/queueDPDK/asni-nat/build/asni-nat -d librte_net_qdma.so -l 4 -n 4 -a 0000:16:00.1 -- -p 0x3 --config="(0,0,4)" --rule_ipv4=../chain/fw_10k --rule_ipv6=../chain/rule_ipv6.db'
+        # },
+        # 'marco_chain': {
+        #     'policies': ["", "-a"],
+        #     'base_command': 'sudo /home/vladimiro/dpdk_patched/marco/dpdk-20.11/queueDPDK/asni-nat/build/asni-nat -d librte_net_qdma.so -l 4 -n 4 -a 0000:16:00.1,indirect_queues=1024 -- -p 0x3 --config="(0,0,4)" --rule_ipv4=../chain/fw_10k --rule_ipv6=../chain/rule_ipv6.db'
+        # },
         'mica': {
-            'policies': ["", "-a", "-a -s 5", "-s 20", "-a -s 20", "-s 50"],
+            'policies': ["", "-a", "-a -s 5", "-s 20", "-a -s 20"],
             'base_command': 'sudo /home/vladimiro/dpdk_patched/queueDPDK/toasty-mica/build/toasty-mica -d librte_net_qdma.so -l 4 -n 4 -a 0000:16:00.1 -- '
         },
         'marco_mica': {
             'policies': ["", "-a"],
-            'base_command': 'sudo /home/vladimiro/dpdk_patched/marco/dpdk-20.11/queueDPDK/toasty-mica/build/toasty-mica -d librte_net_qdma.so -l 4 -n 4 -a 0000:16:00.1 -- '
-        }
+            'base_command': 'sudo /home/vladimiro/dpdk_patched/marco/dpdk-20.11/queueDPDK/toasty-mica/build/toasty-mica -d librte_net_qdma.so -l 4 -n 4 -a 0000:16:00.1,indirect_queues=1024 -- '
+        },
+        # 'mica': {
+        #     'policies': ["", "-a", "-a -s 5"],
+        #     'base_command': 'sudo /home/vladimiro/dpdk_patched/queueDPDK/toasty-mica/build/toasty-mica -d librte_net_qdma.so -l 4 -n 4 -a 0000:16:00.1 -- '
+        # },
     }
     
-    # Zipf skew values
-    zipf_skews = [0.6, 0.9]
     
     # Eventi perf da raccogliere
     perf_events = [
@@ -350,8 +391,9 @@ def main():
     ]
     
     try:
-        print("Connessione a TRex...")
+        tqdm.write("Connessione a TRex...")
         client.connect()
+
         
         # Calcola il numero totale di iterazioni
         total_iterations = sum(
@@ -365,9 +407,9 @@ def main():
         # Loop sui comandi e skew values
         for cmd_name, cmd_config in commands_config.items():
             for zipf_skew in zipf_skews:
-                print(f"\n{'='*80}")
-                print(f"TEST: {cmd_name} | Zipf Skew: {zipf_skew}")
-                print(f"{'='*80}\n")
+                tqdm.write(f"\n{'='*80}")
+                tqdm.write(f"TEST: {cmd_name} | Zipf Skew: {zipf_skew}")
+                tqdm.write(f"{'='*80}\n")
                 
                 # Crea cartella per i risultati
                 results_dir = f"results_{cmd_name}_skew{zipf_skew}"
@@ -381,20 +423,22 @@ def main():
                 if os.path.isfile(csv_file):
                     os.remove(csv_file)
                 
-                print(f"Reset porta 0...")
+                tqdm.write(f"Reset porta 0...")
+
                 client.reset(ports=PORTS)
                 
-                print(f"Carico stream da profilo Zipf {zipf_skew} Python...")
+                tqdm.write(f"Carico stream da profilo Zipf {zipf_skew} Python...")
                 streams = STLProfile.load_py(
                     PROFILE_FILE,
                     direction=0,
                     port_id=0,
                     num_flows=10000,
+                    # num_flows=1,
                     skew=zipf_skew,
                     packet_size=64
                 ).get_streams()
                 
-                print(f"✅ Caricati {len(streams):,} stream dal profilo Zipf\n")
+                tqdm.write(f"✅ Caricati {len(streams):,} stream dal profilo Zipf\n")
                 client.add_streams(streams, ports=PORTS)
                 
                 # CONFIG NDR
@@ -414,7 +458,7 @@ def main():
                 )
                 config.receive_ports = [0]
                 
-                print(f"🚀 Avvio benchmark NDR (zero packet loss)...\n")
+                tqdm.write(f"🚀 Avvio benchmark NDR (zero packet loss)...\n")
                 
                 base_command = cmd_config['base_command']
                 policies = cmd_config['policies']
@@ -422,7 +466,8 @@ def main():
                 for policy in policies:
                     for queue in queues:                
                         for repetition in range(repetitions):
-                            pbar.set_description(f"[{cmd_name:12} | Q:{queue:4d} | {policy:12} | Rep:{repetition+1}/{repetitions}]")
+                            policy_desc = policy if policy else "default"
+                            pbar.set_description(f"[{cmd_name:12} |skew:{zipf_skew:.2f}| Q:{queue:4d} | {policy_desc:12} | Rep:{repetition+1}/{repetitions}]")
                             
                             bench = NdrBench(client, config)
                             process = launch_program(base_command, queue, policy)
@@ -434,7 +479,9 @@ def main():
                             
                             if final_pps:
                                 # Esegui perf per ottenere le metriche
+                                launch_trex(client, final_pps)
                                 perf_metrics = launch_program_with_perf(base_command, queue, policy, perf_events)
+                                stop_trex(client)
                                 # Esporta risultati benchmark + metriche perf in CSV
                                 export_results_to_csv(bench, policy, queue, repetition+1, perf_metrics, perf_events, csv_file)
             
@@ -443,12 +490,12 @@ def main():
                         calculate_and_export_averages(perf_events, csv_file, averages_csv)
                             
                 
-                print(f"\n✅ Risultati salvati in {results_dir}/")
+                tqdm.write(f"\n✅ Risultati salvati in {results_dir}/")
         
         pbar.close()
     
     finally:
-        print("\nDisconnessione...")
+        tqdm.write("\nDisconnessione...")
         client.disconnect()
 
 
